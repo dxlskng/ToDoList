a06525fa204315180870042410893f2b2c9ee5e5
ZANGO <zango@localhost.localdomain>
2023-03-05 14:09:17 +0000

    feat:header,appcomponent. Il s'agit d'ajouter les options fins de tache et tache en cours

---
+++ src/app/end-task/end-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-task',
+  templateUrl: './end-task.component.html',
+  styleUrls: ['./end-task.component.css']
+})
+export class EndTaskComponent {
+
+}
---
+++ src/app/loading-task/loading-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-task',
+  templateUrl: './loading-task.component.html',
+  styleUrls: ['./loading-task.component.css']
+})
+export class LoadingTaskComponent {
+
+}
