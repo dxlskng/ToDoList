5c1a56c914074b6bba05f58bac20109749ae957e
ZANGO <zango@localhost.localdomain>
2023-03-06 14:47:09 +0000

    debut observable

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit{
+  interval$!:Observable<number>
+ngOnInit(): void {
+   this.interval$=interval(1000)
+}
 }
