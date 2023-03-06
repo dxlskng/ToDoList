50780bf6cb9e1cc8011c5561a90110b67fa93e5c
ZANGO <zango@localhost.localdomain>
2023-03-06 17:21:48 +0000

    Use of hight level operator

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, map, Observable } from 'rxjs';
+import { delay, interval, map, mergeMap, Observable, of, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +7,31 @@ import { interval, map, Observable } from 'rxjs';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
-  interval$!:Observable<string>
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 ngOnInit(): void {
-   this.interval$=interval(1000).pipe(
-    map(value => value %2===0 ? 
-      `${value} pair`:
-      ` ${value} impair`)
-   );
+  interval(500).pipe(
+    take(10),
+    map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+    tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+    switchMap(color => this.getTrainObservable$(color)),
+    tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+  ).subscribe();
 }
+
+getTrainObservable$(color: 'rouge' | 'jaune') {
+  const isRedTrain = color === 'rouge';
+  isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+  const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+  console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+  return of({ color, trainIndex }).pipe(
+    delay(isRedTrain ? 5000 : 6000)
+  );
 }
+
+translateColor(color: 'rouge' | 'jaune') {
+  return color === 'rouge' ? 'red' : 'yellow';
+}
+   
+}
+
