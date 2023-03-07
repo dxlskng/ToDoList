080f3d8a8f043fbf202b93588b015dc9f03c2753
ZANGO <zango@localhost.localdomain>
2023-03-07 14:02:37 +0000

    Utilisation de sucribe et de subject

--- src/app/end-task/end-task.component.ts
+++ src/app/end-task/end-task.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject, take, takeUntil, tap } from 'rxjs';
 @Component({
   selector: 'app-end-task',
   templateUrl: './end-task.component.html',
   styleUrls: ['./end-task.component.css']
 })
-export class EndTaskComponent {
-
+export class EndTaskComponent implements OnInit, OnDestroy{
+  destroy$!:Subject<boolean>
+  ngOnInit(): void {
+    this.destroy$=new Subject<boolean>();
+      interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+      ).subscribe();
+  }
+ngOnDestroy(): void {
+  this.destroy$.next(true);
+    
+}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { delay, interval, map, mergeMap, Observable, of, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,31 +6,10 @@ import { delay, interval, map, mergeMap, Observable, of, switchMap, take, tap } 
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
-  redTrainsCalled = 0;
-  yellowTrainsCalled = 0;
 ngOnInit(): void {
-  interval(500).pipe(
-    take(10),
-    map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
-    tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-    switchMap(color => this.getTrainObservable$(color)),
-    tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
-  ).subscribe();
+
 }
 
-getTrainObservable$(color: 'rouge' | 'jaune') {
-  const isRedTrain = color === 'rouge';
-  isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-  const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-  console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-  return of({ color, trainIndex }).pipe(
-    delay(isRedTrain ? 5000 : 6000)
-  );
-}
-
-translateColor(color: 'rouge' | 'jaune') {
-  return color === 'rouge' ? 'red' : 'yellow';
-}
    
 }
 
