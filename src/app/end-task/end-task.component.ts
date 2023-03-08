331fd7a871fe1bba080a28f20deb39dc794c83b0
ZANGO <zango@localhost.localdomain>
2023-03-08 00:54:56 +0000

    use observable

--- src/app/end-task/end-task.component.ts
+++ src/app/end-task/end-task.component.ts
@@ -5,17 +5,12 @@ import { interval, Subject, take, takeUntil, tap } from 'rxjs';
   templateUrl: './end-task.component.html',
   styleUrls: ['./end-task.component.css']
 })
-export class EndTaskComponent implements OnInit, OnDestroy{
-  destroy$!:Subject<boolean>
+export class EndTaskComponent implements OnInit {
   ngOnInit(): void {
-    this.destroy$=new Subject<boolean>();
-      interval(1000).pipe(
-      takeUntil(this.destroy$),
-      tap(console.log)
-      ).subscribe();
+interval(1000).pipe(
+  tap(
+    console.log
+  )
+).subscribe();
   }
-ngOnDestroy(): void {
-  this.destroy$.next(true);
-    
-}
 }
