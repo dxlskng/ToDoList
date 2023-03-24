6bcb8e42b5bd470e08fa6798c642b26f127861a8
ZANGO <zango@localhost.localdomain>
2023-03-24 23:25:55 +0000

    add of fetch from backend method in task.services.ts and sucription of observale

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,13 +1,20 @@
 import {Injectable} from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { Observable } from "rxjs";
 import {taskcomposition} from "../models/taskcomposition.model";
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn:'root'
 })
 export class taskService{
+  constructor(public http: HttpClient){}
 taskList :taskcomposition[]=[
 ];
+/*task$!:Observable<taskcomposition[]>*/
+displaytask():Observable<taskcomposition[]>{
+return this.http.get<taskcomposition[]>('http://localhost:3000/Tache')
+}
+
   addlist(form:FormGroup)
 {
   let titl!:string;
--- src/app/title-and-task-container/title-and-task-container.component.ts
+++ src/app/title-and-task-container/title-and-task-container.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
 import {taskcomposition} from "../models/taskcomposition.model";
 import { taskService } from '../services/task.service';
 
@@ -8,12 +9,13 @@ import { taskService } from '../services/task.service';
   styleUrls: ['./title-and-task-container.component.css']
 })
 export class TitleAndTaskContainerComponent implements OnInit{
-  @Input() mytask!: taskcomposition[];
-
+ /* @Input() mytask!: taskcomposition[];*/
+mytask$!:Observable<taskcomposition[]>
     constructor(public taskservice:taskService ){
 
     };
   ngOnInit() {
-this.mytask =this.taskservice.taskList; 
+/*this.mytask =this.taskservice.taskList; */
+this.mytask$=this.taskservice.displaytask()
 }
 }
